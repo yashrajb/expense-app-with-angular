4b7ed240ddd805a77b1860badbc001050a3f48b2
yashrajb <imyashraj.basan@gmail.com>
2019-02-03 00:30:37 +0530

    path error solved

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.use(express.static(__dirname + '/dist/budget-app'));
 
-app.get("*",function(req,res){
+app.get("/*",function(req,res){
 res.sendFile(path.join(__dirname+'/dist/budget-app/index.html'));
 });
 
